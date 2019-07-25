5137a14b33b872067353c1f6ee3e2b71289f2579
joao_marcolan <jamarcolan@gmail.com>
2019-07-24 21:05:04 -0300

    Primeira versão estavel do site. Está funcionando o layoute inicia, o layout dos cards, e o layoute dos posts. Agora é necessário fazer o ajuste fino e desquebrar o site.
    Co-authored-by: Diego Freitas <41551235+LumusCode@users.noreply.github.com>

--- assets/js/javascript_pagina.js
+++ assets/js/javascript_pagina.js
@@ -26,6 +26,125 @@ class Cards {
 
   geraCard (img_bg, title, content, link = '') {
     //esse método gera um card e retorna o elemento
+    // let div_outer = document.createElement('div');
+    // div_outer.className = 'col-sm-4';
+    
+    let div_inner = document.createElement('div');
+    div_inner.className =  'card-container';
+    
+    let card = document.createElement('div');
+    card.className = 'card';
+    
+    let card_img = document.createElement('div');
+    card_img.className = 'card__img card__img--js';
+    card_img.style.backgroundImage = `url(${img_bg})`;
+    
+    let title1 = document.createElement('div');
+    title1.className = 'card__title';
+    title1.innerHTML = title;
+    
+    let body = document.createElement('p');
+    body.className = 'card__description';
+    body.innerHTML = content;
+
+    let more = document.createElement('p');
+    //let more = document.createElement('div'); caso queira usar como div
+    more.className = 'card__read';
+    more.innerHTML = 'Read more';
+    more.href = link;
+  
+    //encapsular
+
+    card.appendChild(card_img);
+    card.appendChild(title1);
+    card.appendChild(body);
+    card.appendChild(more);
+    div_inner.appendChild(card);
+    // div_outer.appendChild(div_inner);
+    
+    return div_inner;
+  }
+
+  clicou() {
+    window.location.href = this.element.url;
+  }
+}
+
+class Mostra_cards {
+  constructor(url,local_aonde_mostrar)
+  {
+    this.url = url
+    this.local_aonde_mostrar = local_aonde_mostrar
+      this.elementos_pagina =[]
+      fetch( this.url)
+      .then((resp) => resp.json()) // Transform the data into json
+      .then((data)=> {
+        // componente_cartoes.mostra_interface(data)
+        console.log(data)
+        
+        data.forEach(element => {
+          this.mostra(element)
+        });
+        // Create and append the li's to the ul
+        })
+  }
+
+  mostra(element){
+    let ele = new Cards(this.local_aonde_mostrar)
+
+    ele.estruturar_elemento(element)
+
+    this.elementos_pagina.push(ele)
+  }
+}
+
+
+class Mostra_meta extends Mostra_cards {
+  constructor(url,local_aonde_mostrar)
+  {
+    super(url,local_aonde_mostrar)
+  }
+
+  mostra(element){
+    let ele = new Meta(this.local_aonde_mostrar)
+
+    ele.estruturar_elemento(element)
+
+    this.elementos_pagina.push(ele)
+  }
+}
+
+class Meta{
+
+  constructor(element) {
+    this.tagAnchor = element;
+  }
+
+  // precisa melhorar a estrutura dos dados que v'ao para o div.innerHTML
+  estruturar_elemento(element) {
+    this.element = element;
+    
+    /*const div = $(`
+    <div class="col-sm-4">    
+    <div class="card-container flex-wrap space-between">
+      <div class="card">
+        <div class="card__img card__img--js" style="background-image: url('${element.imagem_front}');"></div>
+        <h2 class="card__title">${element.post_title}</h2>
+        <p class="card__description">${element.post_des}</p>
+        <div class="card__read">Read more</div>
+      </div>
+    </div>
+  </div>`).on("click", () => this.clicou())
+    */
+
+    const div = $(`<meta>`)
+    // const div = $(this.geraCard(element.imagem_front, element.post_title, element.post_des)).on("click", () => this.clicou())
+
+    this.tagAnchor.append(div)
+  }
+
+  geraCard (img_bg, title, content, link = '') {
+    //esse método gera um card e retorna o elemento
     let div_outer = document.createElement('div');
     div_outer.className = 'col-sm-4';
     
@@ -65,35 +184,5 @@ class Cards {
     return div_outer;
   }
 
-  clicou() {
-    window.location.href = this.element.url;
-  }
-}
-
-class Mostra_cards {
-  constructor(url,local_aonde_mostrar)
-  {
-    this.url = url
-    this.local_aonde_mostrar = local_aonde_mostrar
-      this.elementos_pagina =[]
-      fetch( this.url)
-      .then((resp) => resp.json()) // Transform the data into json
-      .then((data)=> {
-        // componente_cartoes.mostra_interface(data)
-        console.log(data)
-        
-        data.forEach(element => {
-          this.mostra(element)
-        });
-        // Create and append the li's to the ul
-        })
-  }
-
-  mostra(element){
-    let ele = new Cards(this.local_aonde_mostrar)
-
-    ele.estruturar_elemento(element)
-
-    this.elementos_pagina.push(ele)
-  }
+  
 }
